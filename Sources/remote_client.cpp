064543b6003af0b1b081b9b657f9e4d61dd56a32
bijanbina <bijanbina@gmail.com>
2024-08-11 17:26:17 +0200

    Refactor RemoteClient to Match both PC and Server Side

---
+++ Sources/remote_client.cpp
@@ -0,0 +1,57 @@
+#include "remote_client.h"
+
+ScRemoteClient::ScRemoteClient(QObject *parent):
+    QObject(parent)
+{
+    remote = new QTcpSocket();
+    connect(remote, SIGNAL(disconnected()),
+            this,   SLOT  (disconnected()));
+    connect(remote, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(displayError(QAbstractSocket::SocketError)));
+}
+
+void ScRemoteClient::open()
+{
+    remote->connectToHost(QHostAddress(ScSetting::remote_host),
+                          ScSetting::tx_port);
+}
+
+void ScRemoteClient::writeBuf(QHostAddress host)
+{
+    remote->connectToHost(host, ScSetting::tx_port);
+    remote->waitForConnected();
+    if( remote->isOpen()==0 )
+    {
+        qDebug() << "WriteBuf: failed connection not opened";
+        return;
+    }
+    remote->setSocketOption(QAbstractSocket::LowDelayOption, 1);
+
+    int s = remote->write(buf);
+    qDebug() << "writeBuf:" << buf.length() << s;
+    buf.clear();
+
+    remote->disconnectFromHost();
+    remote->waitForDisconnected();
+    remote->close();
+}
+
+void ScRemoteClient::disconnected()
+{
+    remote->close();
+    buf.clear();
+    qDebug() << "ScRemoteClient: Disconnected";
+}
+
+void ScRemoteClient::displayError(
+        QAbstractSocket::SocketError socketError)
+{
+//    if( socketError==QTcpSocket::RemoteHostClosedError )
+//    {
+//        return;
+//    }
+
+    qDebug() << "Network error The following error occurred"
+             << remote->errorString();
+    remote->close();
+}
---
+++ Sources/remote_client.h
@@ -0,0 +1,25 @@
+#ifndef SC_REMOTECLIENT_H
+#define SC_REMOTECLIENT_H
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include "backend.h"
+
+class ScRemoteClient : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ScRemoteClient(QObject *parent = nullptr);
+    void open();
+    void writeBuf(QHostAddress host);
+
+    QTcpSocket *remote;
+    QByteArray  buf;
+    int         direct;
+
+private slots:
+    void disconnected();
+    void displayError(QAbstractSocket::SocketError socketError);
+};
+
+#endif // SC_REMOTECLIENT_H
